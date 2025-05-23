1a3b9bf11c705cf31287c8fc7015345a9872fd0b
Deen Abiola <sir.deenicus@gmail.com>
2025-05-23 02:24:15 +0100

    Initialize project structure with .gitignore, solution, and F# project files

---
+++ Map.fs
@@ -0,0 +1,20 @@
+﻿namespace AspectGameEngine
+
+//struct 
+[<Struct>]
+type TileImageLoc =
+    val AtlasIndex: int
+    val Row: int
+    val Column: int
+    new(atlasIndex, row, column) = { AtlasIndex = atlasIndex; Row = row; Column = column }
+
+type Item = 
+//struct record
+[<Struct>] type Tile = { 
+    
+
+}
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
